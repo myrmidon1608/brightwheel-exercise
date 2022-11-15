1da69b837f0ca29bb4fe244c0c94ba835706d23f
NME7 <nicholas.moore@3ds.com>
2022-11-15 13:48:31 -0800

    Added node.js api strawman

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# brightwheel-exercise
+# Brightwheel
+
+### Dependencies
+- npm -v
+- node -v
+- npm install -g express-generator
+
+### Run server
+
+http://localhost:3000/api/
+
+> node app.js
---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require('express'),
+    app = express(),
+    routes = require('./routes');
+api = require('./routes/api');
+port = process.env.PORT || 3000;
+
+// Page Routes
+app.get('/', routes.index);
+
+// API Routes
+app.get('/api/name', api.name);
+
+app.listen(port, function () {
+    console.log('Listening on port ' + port);
+});
